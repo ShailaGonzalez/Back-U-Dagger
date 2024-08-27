347432e2d95d38771d271440fee1b5218129734f
ShailaGonzalez <shailagonzalezcarrero@gmail.com>
2024-08-27 09:52:59 +0200

    upload changes

--- src/main/java/com/skate/skate/controllers/Userscontroller.java
+++ src/main/java/com/skate/skate/controllers/Userscontroller.java
@@ -8,9 +8,12 @@ import com.skate.skate.models.Users;
 import com.skate.skate.services.UsersService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class Userscontroller {
@@ -28,8 +31,22 @@ public class Userscontroller {
     }
 
     @PostMapping("/users") /// hacer el post en postman y comprobarlo en pgadmin
-    public ResponseEntity<Object> addUsers(@RequestBody Users Users) {
-        return usersService.addUsers(Users);
+    public ResponseEntity<Object> addUsers(@RequestBody Users users) {
+        return usersService.addUsers(users);
+    }
+    
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable Integer id) {
+        return usersService.getUserById(id);
     }
 
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Integer id, @RequestBody Users updatedUser) {
+        return usersService.updateUser(id, updatedUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Integer id) {
+        return usersService.deleteUser(id);
+    }
 }
