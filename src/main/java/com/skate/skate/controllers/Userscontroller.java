eb6f6f8a2133a6b74542427e6b8ff685728a490e
ShailaGonzalez <shailagonzalezcarrero@gmail.com>
2024-08-27 08:44:37 +0200

    add missing files

--- src/main/java/com/skate/skate/controllers/Userscontroller.java
+++ src/main/java/com/skate/skate/controllers/Userscontroller.java
@@ -12,32 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
-
-
-
 @RestController
 public class Userscontroller {
-    
 
     private final UsersService usersService;
 
-    public Userscontroller(UsersService usersService){
-        this.usersService= usersService;
+    public Userscontroller(UsersService usersService) {
+        this.usersService = usersService;
 
     }
 
-
-    @GetMapping("/users") ///hacer el get en postman
-    public List<Users> getUsers(){
+    @GetMapping("/users") /// hacer el get en postman
+    public List<Users> getUsers() {
         return usersService.getUsers();
-        }
- 
-    @PostMapping("/users")  ///hacer el post en postman y comprobarlo en pgadmin
-    public ResponseEntity<Object> addUsers(@RequestBody Users Users){
-            return usersService.addUsers(Users);
-            }
+    }
 
+    @PostMapping("/users") /// hacer el post en postman y comprobarlo en pgadmin
+    public ResponseEntity<Object> addUsers(@RequestBody Users Users) {
+        return usersService.addUsers(Users);
+    }
 
 }
